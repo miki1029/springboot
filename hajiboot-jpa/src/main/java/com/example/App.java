2f14d0742c9d5c327635dad8340eeb9e87ebeb1e
miki1029 <miki1029g@gmail.com>
2015-10-16 14:16:50 +0900

    Pageable, PageRequest, Page를 이용한 페이징 처리

--- hajiboot-jpa/src/main/java/com/example/App.java
+++ hajiboot-jpa/src/main/java/com/example/App.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Created by miki on 15. 10. 10..
@@ -24,8 +27,14 @@ public class App implements CommandLineRunner {
                 null, "민우", "김"));
         System.out.println(created + " is created!");
 
-        // 데이터 표시
-        customerRepository.findAllOrderByName().forEach(System.out::println);
+        // 페이징 처리
+        Pageable pageable = new PageRequest(0, 3); // (가져올 페이지 수, 한 페이지가 포함하는 데이터 수)
+        Page<Customer> page = customerRepository.findAll(pageable);
+        System.out.println("한 페이지당 데이터 수 = " + page.getSize());
+        System.out.println("현재 페이지 = " + page.getNumber()); // 페이지는 0부터 시작
+        System.out.println("전체 페이지 수 = " + page.getTotalPages());
+        System.out.println("전체 데이터 수 = " + page.getTotalElements());
+        page.getContent().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
