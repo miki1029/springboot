7fa768ebd5dcc34902c9bf697b74138d569957ee
miki1029 <miki1029g@gmail.com>
2015-10-16 14:00:05 +0900

    RowMapper 람다 표현식으로 변경

--- hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -5,13 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * Created by miki on 15. 10. 10..
@@ -29,12 +25,9 @@ public class App implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
         SqlParameterSource param = new MapSqlParameterSource().addValue("id", 1);
-        Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-            @Override
-            public Customer mapRow(ResultSet resultSet, int i) throws SQLException {
+        Customer result = jdbcTemplate.queryForObject(sql, param, (resultSet, rowNum) -> {
                 return new Customer(
                         resultSet.getInt("id"), resultSet.getString("first_name"), resultSet.getString("last_name"));
-            }
         });
 
         System.out.println("result = " + result);
