afaa9c550a1436c14893612112caff42feca0216
miki1029 <miki1029g@gmail.com>
2015-10-16 14:24:20 +0900

    POST, PUT, DELETE method API 추가

--- hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -3,10 +3,8 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,26 @@ public class CustomerRestController {
     Customer getCustomer(@PathVariable Integer id) {
         Customer customer = customerService.findOne(id);
         return customer;
+    }
+
+    // 신규 고객 정보 작성
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED) // 없으면 : 200 OK, CREATED : 201 CREATED
+    Customer postCustomers(@RequestBody Customer customer) { // HTTP Request를 @RequestBody 객체로 받음
+        return customerService.create(customer);
+    }
+
+    // 고객 한 명의 정보 업데이트
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return customerService.update(customer);
+    }
+
+    // 고객 한 명의 정보 삭제
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT) // NO_CONTENT : 204 NO_CONTENT
+    void deleteCustomer(@PathVariable Integer id) {
+        customerService.delete(id);
     }
 }
