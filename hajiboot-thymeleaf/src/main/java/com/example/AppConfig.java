38a02d80573ae14c6ac4f544a2072d3d900e21c4
miki1029 <miki1029g@gmail.com>
2015-10-16 21:24:50 +0900

    [dummy] UTF-8 넣으니 오히려 안되서 임시 주석, list.html 코드 정렬

--- hajiboot-thymeleaf/src/main/java/com/example/AppConfig.java
+++ hajiboot-thymeleaf/src/main/java/com/example/AppConfig.java
@@ -40,12 +40,12 @@ public class AppConfig {
         return new Log4jdbcProxyDataSource(this.dataSource);
     }
 
-    @Order(Ordered.HIGHEST_PRECEDENCE) // Bean 정의의 우선순위(필터 적용 순서로 값이 작은 쪽이 먼저 적용)(Integer.MIN_VALUE)
+/*    @Order(Ordered.HIGHEST_PRECEDENCE) // Bean 정의의 우선순위(필터 적용 순서로 값이 작은 쪽이 먼저 적용)(Integer.MIN_VALUE)
     @Bean
     CharacterEncodingFilter characterEncodingFilter() {
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
         filter.setForceEncoding(true);
         return filter;
-    }
+    }*/
 }
